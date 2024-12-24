39c9b3b90b92aa00503ae655f9d871eebc768bb4
veronic28 <156885216+AshurovMagamed@users.noreply.github.com>
2024-12-24 09:33:10 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryExam
+{
+    public class Student
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private double ocenka;
+        public double Ocenka
+        {
+            get { return ocenka; }
+            set { ocenka = value; }
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Имя {Name}, Оценка {Ocenka}");
+        }
+    }
+}
